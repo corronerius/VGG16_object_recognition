de156650b097a6cd55acd39f3f488a5eaed8150e
Andrii Kolotov <andrii.kolotov@gmail.com>
2019-05-17 10:08:56 +0300

    vgg16_object_classification

---
+++ vgg16_object_classification.py
@@ -0,0 +1,33 @@
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+import urllib
+from PIL import Image
+import requests
+from io import BytesIO
+
+# Создаем модель с архитектурой VGG16 и загружаем веса, обученные
+# на наборе данных ImageNet
+model = VGG16(weights='imagenet')
+
+# Загружаем изображение для распознавания, преобразовываем его в массив
+# numpy и выполняем предварительную обработку
+#img_path = '400.jpg'
+url = 'https://picsum.photos/300/400'
+img_from_web = open('400.jpg','wb')
+img_from_web.write(requests.get(url).content)
+img_from_web.close()
+
+img_path = '400.jpg'
+
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+# Запускаем распознавание объекта на изображении
+preds = model.predict(x)
+
+# Печатаем три класса объекта с самой высокой вероятностью
+print('Результаты распознавания:', decode_predictions(preds, top=3)[0])
